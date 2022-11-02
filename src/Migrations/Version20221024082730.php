65ed092ff7870bddf63517c9059609d7af7ca73b
Steinwender, Tamara <tamara.steinwender@tugraz.at>
2022-11-02 14:05:22 +0100

    Update src/Migrations/Version20221024082730.php

--- src/Migrations/Version20221024082730.php
+++ src/Migrations/Version20221024082730.php
@@ -14,16 +14,16 @@ final class Version20221024082730 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Create table blob_connector_local';
+        return 'Create table blob_connector_filesystem';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE blob_connector_local (identifier INT AUTO_INCREMENT NOT NULL, valid_until DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', link VARCHAR(255) NOT NULL, file_data_identifier VARCHAR(255) NOT NULL, PRIMARY KEY(identifier)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE blob_connector_filesystem (identifier INT AUTO_INCREMENT NOT NULL, valid_until DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', link VARCHAR(255) NOT NULL, file_data_identifier VARCHAR(255) NOT NULL, PRIMARY KEY(identifier)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE blob_connector_local');
+        $this->addSql('DROP TABLE blob_connector_filesystem');
     }
 }
