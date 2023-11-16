8dd2a318f1c94b92c92cabd53e64a6fc437c30c5
Christoph Reiter <reiter.christoph@gmail.com>
2023-11-16 13:37:26 +0100

    tests: stop using legacy api-platform aliases

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BlobConnectorFilesystemBundle\Tests;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle;
+use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
 use Dbp\Relay\BlobConnectorFilesystemBundle\DbpRelayBlobConnectorFilesystemBundle;
 use Dbp\Relay\CoreBundle\DbpRelayCoreBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
