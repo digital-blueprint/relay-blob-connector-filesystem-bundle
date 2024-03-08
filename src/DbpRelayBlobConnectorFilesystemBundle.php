76e6785987543537475147723232a19dc9b57b24
Christoph Reiter <reiter.christoph@gmail.com>
2024-03-08 18:46:21 +0100

    Add support for Symfony 6
    Switch to dev-main for relay-blob-bundle for now, as there is no release
    supporting Symfony 6 yet.

--- src/DbpRelayBlobConnectorFilesystemBundle.php
+++ src/DbpRelayBlobConnectorFilesystemBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DbpRelayBlobConnectorFilesystemBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
     }
 }
